439b54e7031c45afb1b74bcd26fd158851bf158c
Zwivhuya6132 <113597052+Zwivhuya6132@users.noreply.github.com>
2023-04-30 17:06:59 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,35 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function nextSlide() {
+  showSlide(slideIndex += 1);
+}
+
+function prevSlide() {
+  showSlide(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+  showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// automatic slideshow
+setInterval(() => {
+  nextSlide();
+}, 5000);
